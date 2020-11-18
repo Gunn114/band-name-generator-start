0e3198e96297c7895bd64d66d95146e5dbe94162
Gunn114 <techxcheck88@gmail.com>
2020-11-18 15:44:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#1. Create a greeting for your program.
+print("Hey there Rockstar! welcome to the Band Name Generator")
+
+#2. Ask the user for their favorite food/dish.
+food = input("What's the name of your favorite food or dish?\n")
+
+#3. Ask the user for the name of a vehicle.
+vehicle = input("what's your car or fav car make?...or are you a motorcycle rebel?\n")
+
+#4. Combine the name of their food/dish and vehicle and show them their band name.
+print("your awesome band name is " + food + " " + vehicle)
+
+#5. Make sure the input cursor shows on a new line = \n
